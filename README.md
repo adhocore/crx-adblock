0d527e82aa8c1e232bbbabaab420d448f5eb9673
Jitendra Adhikari <jiten.adhikary@gmail.com>
2015-09-08 12:09:57 +0545

    revamp
    add more rules. no longer need to play with manifest.json. killing ads
    is as simple as adding rules just

--- README.md
+++ README.md
@@ -2,5 +2,13 @@
 
 - chrome web browser extension
 - hide/block/remove custom forceful banners from any webpage
-- first setup permissions in manifest.json and add rules in rules.json
+- add rules in rules.json
 
+# rules (how-to)
+
+```json
+"hostname": {
+	"query":[".selector1,.selector2", "#selector3"],
+	"call": "some callback function name"
+}
+```
--- adblock.js
+++ adblock.js
@@ -7,6 +7,7 @@
 ;
 var Adblock = Adblock || {
   fire: function() {
+    console.log(0);
     var xhr = new XMLHttpRequest();
       xhr.open("GET", chrome.extension.getURL('/rules.json'), true);
       xhr.onload = this.block.bind(this);
@@ -16,19 +17,23 @@ var Adblock = Adblock || {
     var config = xhrEvt.target.responseText;
         config = JSON.parse(config);
         config = config[document.location.hostname];
+        console.log(1);
     if (!config) {
       return;
     }
+    console.log(2);
     for (var key in config) {
       if (key === 'call') {
         config[key].forEach(function(cb) {
           try { cb.call(); } catch (e) {}
         });
       } else {
+        console.log(3);
         el = config[key].join(',');
         chrome.runtime.sendMessage({
           elm: el
         });
+        console.log(el);
         try {
           els = document.querySelectorAll(el);
           for (var i=0; i<els.length; i++) {
